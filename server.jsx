5bb9cffd6aef3a329e524f07ae425dd0f1a4cc00
Brian Jungk <brian.jungk@outlook.com>
2021-04-13 22:54:09 -0500

    test1 no static

--- server.jsx
+++ server.jsx
@@ -44,7 +44,8 @@ import  styles  from './vno-ssr/style.js'
 
 // You need to import `h` factory function as Deno Deploy
 // uses it instead of `React.createElement`
-
+import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
 function App() {
   return (
     <html>
@@ -60,7 +61,7 @@ function App() {
 
 addEventListener("fetch", (event) => {
   // renderToString generates html string from JSX components.
-  const response = new Response(vueServerRenderer(<App />), {
+  const response = new Response(renderToString(<App />), {
     headers: { "content-type": "text/html; charset=uft-8" },
   });
 
