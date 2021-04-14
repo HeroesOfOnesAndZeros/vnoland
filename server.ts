c7fad34ce0ee2dd32762415d1d8a15ba029ae53f
Brian Jungk <brian.jungk@outlook.com>
2021-04-13 19:16:35 -0500

    test1 no static

--- server.ts
+++ server.ts
@@ -1,98 +1,85 @@
-// import { opine, serveStatic } from "https://deno.land/x/opine@1.2.0/mod.ts";
-// import  vueServerRenderer from 'https://deno.land/x/vue_server_renderer@/mod.js';
+import { opine, serveStatic } from "https://deno.land/x/opine@1.2.0/mod.ts";
+import  vueServerRenderer from 'https://deno.land/x/vue_server_renderer@/mod.js';
 
-// import App from './vno-ssr/build.js';
-// import { join, dirname, resolve } from "https://deno.land/std@0.63.0/path/mod.ts";
-// import  styles  from './vno-ssr/style.js'
+import App from './vno-ssr/build.js';
+import { join, dirname, resolve } from "https://deno.land/std@0.63.0/path/mod.ts";
+import  styles  from './vno-ssr/style.js'
 
-// const port = 3000
-// // const app = opine();
+const port = 3000
+// const app = opine();
 
-// //app.use(serveStatic('vno-build'));
-// // app.use(serveStatic(resolve('vno-build')));
-// // app.use(serveStatic(resolve('src/assets')))
-//  const __dirname = dirname(import.meta.url);
+//app.use(serveStatic('vno-build'));
+// app.use(serveStatic(resolve('vno-build')));
+// app.use(serveStatic(resolve('src/assets')))
+ const __dirname = dirname(import.meta.url);
 
 
 
-// //app.use("/", (req, res, next) => {
+//app.use("/", (req, res, next) => {
       
-//       // let rendered;
-//       // vueServerRenderer(App, (err:any, res:any) => {
-//       //   rendered = res;
-//       // });
+      // let rendered;
+      // vueServerRenderer(App, (err:any, res:any) => {
+      //   rendered = res;
+      // });
       
-//       // const html =
-//       // `<html>
-//       //    <head>
+      // const html =
+      // `<html>
+      //    <head>
          
-//       //       ${styles}
+      //       ${styles}
            
-//       //    </head>
-//       //    <body>
-//       //      <div id="root">${rendered}</div>
-//       //      <script type="module" src="./build.js"></script>
-//       //    </body>
-//       //  </html>`;
+      //    </head>
+      //    <body>
+      //      <div id="root">${rendered}</div>
+      //      <script type="module" src="./build.js"></script>
+      //    </body>
+      //  </html>`;
 
-//     //res.type("text/html").send(html);
-//   //});
+    //res.type("text/html").send(html);
+  //});
 
-// // app.listen({ port });
+// app.listen({ port });
   
-// //console.log(`Vue SSR App listening on port ${port}`);
+//console.log(`Vue SSR App listening on port ${port}`);
 
 
-//   function App(){
-//     let rendered;
-//     vueServerRenderer(App, (err:any, res:any) => {
-//       rendered = res;
-//     });
+  // function App(){
+  //   let rendered;
+  //   vueServerRenderer(App, (err:any, res:any) => {
+  //     rendered = res;
+  //   });
     
-//     const html =
-//     `<html>
-//        <head>
+  //   const html =
+  //   `<html>
+  //      <head>
        
-//           ${styles}
+  //         ${styles}
          
-//        </head>
-//        <body>
-//          <div id="root">${rendered}</div>
-//          <script type="module" src="./build.js"></script>
-//        </body>
-//      </html>`;
-//      return html
-//   }
-
-//   addEventListener("fetch", (event) => {
-//     const response = new Response(App(), {
-//       headers: { "content-type": "text/plain" },
-//     });
-//     event.respondWith(response);
-//   });
+  //      </head>
+  //      <body>
+  //        <div id="root">${rendered}</div>
+  //        <script type="module" src="./build.js"></script>
+  //      </body>
+  //    </html>`;
+  //    return html
+  // }
 
 
-import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
-
-function App() {
-  return (
-    <html>
-      <head>
-        <title>Hello from JSX</title>
-      </head>
-      <body>
-        <h1>Hello world</h1>
-      </body>
-    </html>
-  );
-}
-
-addEventListener("fetch", (event) => {
-  // renderToString generates html string from JSX components.
-  const response = new Response(renderToString(<App />), {
-    headers: { "content-type": "text/html; charset=uft-8" },
+  addEventListener("fetch", (event) => {
+    // renderToString generates html string from JSX components.
+    const response = new Response(vueServerRenderer(App, (res:any)=> {
+      return res
+    }), {
+      headers: { "content-type": "text/html; charset=uft-8" },
+    });
+  
+    event.respondWith(response);
   });
+  
+  // addEventListener("fetch", (event) => {
+  //   const response = new Response(App(), {
+  //     headers: { "content-type": "text/plain" },
+  //   });
+  //   event.respondWith(response);
+  // });
 
-  event.respondWith(response);
-});
