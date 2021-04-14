ad6ed2bba0c7f56d3bb6eed4adb6c59628b8a6ad
Brian Jungk <brian.jungk@outlook.com>
2021-04-13 22:52:01 -0500

    test1 no static

---
+++ server.jsx
@@ -0,0 +1,68 @@
+import { opine, serveStatic } from "https://deno.land/x/opine@1.2.0/mod.ts";
+import  vueServerRenderer from 'https://deno.land/x/vue_server_renderer@/mod.js';
+
+import App from './vno-ssr/build.js';
+import { join, dirname, resolve } from "https://deno.land/std@0.63.0/path/mod.ts";
+import  styles  from './vno-ssr/style.js'
+
+//const port = 3000
+// const app = opine();
+
+//app.use(serveStatic('vno-build'));
+// app.use(serveStatic(resolve('vno-build')));
+// app.use(serveStatic(resolve('src/assets')))
+ ///const __dirname = dirname(import.meta.url);
+
+
+
+//app.use("/", (req, res, next) => {
+      
+      // let rendered;
+      // vueServerRenderer(App, (err:any, res:any) => {
+      //   rendered = res;
+      // });
+      
+      // const html =
+      // `<html>
+      //    <head>
+         
+      //       ${styles}
+           
+      //    </head>
+      //    <body>
+      //      <div id="root">${rendered}</div>
+      //      <script type="module" src="./build.js"></script>
+      //    </body>
+      //  </html>`;
+
+    //res.type("text/html").send(html);
+  //});
+
+// app.listen({ port });
+  
+//console.log(`Vue SSR App listening on port ${port}`);
+
+// You need to import `h` factory function as Deno Deploy
+// uses it instead of `React.createElement`
+
+function App() {
+  return (
+    <html>
+      <head>
+        {styles}
+      </head>
+      <body>
+        {rendered}
+      </body>
+    </html>
+  );
+}
+
+addEventListener("fetch", (event) => {
+  // renderToString generates html string from JSX components.
+  const response = new Response(vueServerRenderer(<App />), {
+    headers: { "content-type": "text/html; charset=uft-8" },
+  });
+
+  event.respondWith(response);
+});
