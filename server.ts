a144e81dc7c835d38553baa006ba29d421ba91f9
Brian Jungk <brian.jungk@outlook.com>
2021-04-13 22:25:47 -0500

    test1 no static

--- server.ts
+++ server.ts
@@ -5,13 +5,13 @@ import App from './vno-ssr/build.js';
 import { join, dirname, resolve } from "https://deno.land/std@0.63.0/path/mod.ts";
 import  styles  from './vno-ssr/style.js'
 
-const port = 3000
+//const port = 3000
 // const app = opine();
 
 //app.use(serveStatic('vno-build'));
 // app.use(serveStatic(resolve('vno-build')));
 // app.use(serveStatic(resolve('src/assets')))
- const __dirname = dirname(import.meta.url);
+ ///const __dirname = dirname(import.meta.url);
 
 
 
@@ -42,44 +42,30 @@ const port = 3000
   
 //console.log(`Vue SSR App listening on port ${port}`);
 
+// You need to import `h` factory function as Deno Deploy
+// uses it instead of `React.createElement`
 
-  // function App(){
-  //   let rendered;
-  //   vueServerRenderer(App, (err:any, res:any) => {
-  //     rendered = res;
-  //   });
-    
-  //   const html =
-  //   `<html>
-  //      <head>
-       
-  //         ${styles}
-         
-  //      </head>
-  //      <body>
-  //        <div id="root">${rendered}</div>
-  //        <script type="module" src="./build.js"></script>
-  //      </body>
-  //    </html>`;
-  //    return html
-  // }
+import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
 
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
 
-  addEventListener("fetch", (event) => {
-    // renderToString generates html string from JSX components.
-    const response = new Response(vueServerRenderer(App, (err:any, res:any)=> {
-      return res
-    }), {
-      headers: { "content-type": "text/html; charset=uft-8" },
-    });
-  
-    event.respondWith(response);
+addEventListener("fetch", (event) => {
+  // renderToString generates html string from JSX components.
+  const response = new Response(renderToString(<App />), {
+    headers: { "content-type": "text/html; charset=uft-8" },
   });
-  
-  // addEventListener("fetch", (event) => {
-  //   const response = new Response(App(), {
-  //     headers: { "content-type": "text/plain" },
-  //   });
-  //   event.respondWith(response);
-  // });
 
+  event.respondWith(response);
+});
