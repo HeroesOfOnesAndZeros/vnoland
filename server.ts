af43743ec8c0961bdfe5f45df02c7a970c75f5a1
Brian Jungk <brian.jungk@outlook.com>
2021-04-13 22:27:28 -0500

    test1 no static

--- server.ts
+++ server.ts
@@ -52,10 +52,10 @@ function App() {
   return (
     <html>
       <head>
-        <title>Hello from JSX</title>
+        <title>Hello</title>
       </head>
       <body>
-        <h1>Hello world</h1>
+        <h1>Hello</h1>
       </body>
     </html>
   );
